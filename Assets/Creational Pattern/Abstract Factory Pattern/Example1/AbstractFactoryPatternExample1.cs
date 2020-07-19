e799d03e095736689b2c21335f7702fac534c213
张浩材 <435504339@qq.com>
2020-07-20 00:30:00 +0800

    Update Abstract Factory Pattern

---
+++ Assets/Creational Pattern/Abstract Factory Pattern/Example1/AbstractFactoryPatternExample1.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbstractFactoryPatternExample1
+{
+    public class AbstractFactoryPatternExample1 : MonoBehaviour
+    {
+        void Start()
+        {
+            ConcreteFactory1 f1 = new ConcreteFactory1();
+            ConcreteFactory2 f2 = new ConcreteFactory2();
+
+            Client c1 = new Client(f1);
+            c1.LogAll();
+
+            Client c2 = new Client(f2);
+            c2.LogAll();
+        }
+    }
+
+    public class Client
+    {
+        private ProductA pa;
+        private ProductB pb;
+
+        public Client(AbstractFactory f)
+        {
+            pa = f.CreateProdutA();
+            pb = f.CreateProdutB();
+        }
+
+        public void LogAll()
+        {
+            pa.Log();
+            pb.Log();
+        }
+    }
+
+    /// <summary>
+    /// Factory基类,定义了创建产品A和产品B两个函数
+    /// </summary>
+    public abstract class AbstractFactory
+    {
+        public abstract ProductA CreateProdutA();
+        public abstract ProductB CreateProdutB();
+    }
+
+    /// <summary>
+    /// ConcreteFactory类
+    /// </summary>
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public override ProductA CreateProdutA()
+        {
+            return new ProductA1();
+        }
+
+        public override ProductB CreateProdutB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    public class ConcreteFactory2 : AbstractFactory
+    {
+        public override ProductA CreateProdutA()
+        {
+            return new ProductA2();
+        }
+
+        public override ProductB CreateProdutB()
+        {
+            return new ProductB2();
+        }
+    }
+
+    public abstract class ProductA
+    {
+        public abstract void Log();
+    }
+
+    class ProductA1 : ProductA
+    {
+        public override void Log()
+        {
+            Debug.Log("ProductA1");
+        }
+    }
+
+    class ProductA2 : ProductA
+    {
+        public override void Log()
+        {
+            Debug.Log("ProductA2");
+        }
+    }
+
+    public abstract class ProductB
+    {
+        public abstract void Log();
+    }
+
+    class ProductB1 : ProductB
+    {
+        public override void Log()
+        {
+            Debug.Log("ProductB1");
+        }
+    }
+
+    class ProductB2 : ProductB
+    {
+        public override void Log()
+        {
+            Debug.Log("ProductB2");
+        }
+    }
+}
+
---
+++ Assets/Creational Pattern/Factory Method Pattern/Example1/FactoryMethodPatternExample1.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FactoryMethodPatternExample1
+{
+    public class FactoryMethodPatternExample1 : MonoBehaviour
+    {
+        private void Start()
+        {
+            ProductFactory factory_a = new ProductFactoryA();
+            factory_a.CreateProduct();
+            ProductFactory factory_b = new ProductFactoryB();
+            factory_b.CreateProduct();
+
+            foreach(var p in factory_a.products)
+            {
+                p.Log();
+            }
+
+            foreach (var p in factory_b.products)
+            {
+                p.Log();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Product基类
+    /// </summary>
+    public abstract class Product
+    {
+        public abstract void Log();
+    }
+
+    /// <summary>
+    /// 下列3个都是ConcreteProduct类
+    /// </summary>
+    public class NullProduct : Product
+    {
+        public override void Log()
+        {
+            Debug.Log("This is NullProduct.");
+        }
+    }
+
+    public class ConcreteProductA : Product
+    {
+        public override void Log()
+        {
+            Debug.Log("This is ConcreteProductA.");
+        }
+    }
+
+    public class ConcreteProductB : Product
+    {
+        public override void Log()
+        {
+            Debug.Log("This is ConcreteProductB.");
+        }
+    }
+
+    public abstract class ProductFactory
+    {
+        protected List<Product> _products = new List<Product>();
+
+        public List<Product> products
+        {
+            get { return _products; }
+        }
+
+        public abstract void CreateProduct();
+    }
+
+    public class ProductFactoryA : ProductFactory
+    {
+        public override void CreateProduct()
+        {
+            _products.Add(new ConcreteProductA());
+            _products.Add(new ConcreteProductB());
+            _products.Add(new NullProduct());
+        }
+    }
+
+    public class ProductFactoryB : ProductFactory
+    {
+        public override void CreateProduct()
+        {
+            _products.Add(new ConcreteProductB());
+            _products.Add(new NullProduct());
+            _products.Add(new ConcreteProductA());
+        }
+    }
+}
+
